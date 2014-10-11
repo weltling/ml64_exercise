675d9ea521a984ab5f4b54faa4c52868d7489ca0
Anatol Belski <ab@php.net>
2014-10-11 13:09:27 +0200

    use debug by default

--- djb/make.bat
+++ djb/make.bat
@@ -1,7 +1,10 @@
 
 del djb.obj djb_asm.obj djb.exe djb_bench.exe
 
-ml64 /c /Fodjb_asm.obj djb.asm
-cl /O2 /GL djb.c /link djb_asm.obj
-cl /O2 /GL djb_bench.c /link djb_asm.obj
+rem ml64 /c /Fodjb_asm.obj djb.asm
+rem cl /O2 /GL djb.c /link djb_asm.obj
+rem cl /O2 /GL djb_bench.c /link djb_asm.obj
 
+ml64 /c /Zi /Fodjb_asm.obj djb.asm
+cl /Zi djb.c /link djb_asm.obj
+cl /Zi /GL djb_bench.c /link djb_asm.obj
